62e84e19903aceaa4308a22ea021b7ae95723959
THANYA30 <thanyas6363@gmail.com>
2025-04-17 11:09:01 +0530

    Update class.java

--- class.java
+++ class.java
@@ -1,7 +1,6 @@
 public class class {
-  public static void main(string args[]) 
-  {
-    System.out.println("i want you");
+  public static void main(String args[]) {
+  System.out.println("i want you");
   }
 }
 
